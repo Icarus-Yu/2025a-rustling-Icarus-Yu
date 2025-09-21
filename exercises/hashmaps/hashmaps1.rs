960eae81ff4079ad06f6b158db98c948b50e2182
Icarus-Yu <hualuck7@gmail.com>
2025-09-21 21:35:56 +0800

    哈希值部分

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -7,20 +7,21 @@
 // of all the fruits should be at least five.
 //
 // Make me compile and pass the tests!
-//
+//哈希映射的练习 ，键值对的存储结构，通过独一无二的见来快速查找与之相关联的值
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
-
+    basket.insert(String::from("apple"), 2);
+    basket.insert(String::from("orange"), 2);
+    //键必须独一无二
     // TODO: Put more fruits in your basket here.
 
     basket
@@ -34,11 +35,13 @@ mod tests {
     fn at_least_three_types_of_fruits() {
         let basket = fruit_basket();
         assert!(basket.len() >= 3);
+        //键值对的数量必须大于三
     }
 
     #[test]
     fn at_least_five_fruits() {
         let basket = fruit_basket();
         assert!(basket.values().sum::<u32>() >= 5);
+        //值的总和必须大于五
     }
 }
